a84e692ee8111669417bf863a09528ea2f194b70
Nebo Milic <nebo.milic@gmail.com>
2023-05-05 07:46:59 +0200

    Show edit link to detail view if user owns the collection

--- src/app/shared/components/collection-details/collection-details.component.ts
+++ src/app/shared/components/collection-details/collection-details.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core'
+import { AfterViewInit, Component } from '@angular/core'
 import { Collection } from 'src/app/types'
+import { MOCK_USER_ID } from '../../services/mock'
 
 @Component({
     selector: 'app-collection-details',
     templateUrl: './collection-details.component.html',
     styleUrls: ['./collection-details.component.scss'],
 })
-export class CollectionDetailsComponent {
+export class CollectionDetailsComponent implements AfterViewInit {
     selectedCollection: Collection | null = null
+    editable = false
+
+    ngAfterViewInit(): void {
+        if (this.selectedCollection?.authorId == MOCK_USER_ID) {
+            this.editable = true
+        }
+    }
 }
