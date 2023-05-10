0cc2979cb6fd418822d3a7ea36a4d776d8d2146c
Nebo Milic <nebo.milic@gmail.com>
2023-05-10 22:28:41 +0200

    Use material ui for page frame

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,8 +20,14 @@ import { environment } from '../environments/environment'
 import { provideAuth, getAuth } from '@angular/fire/auth'
 import { provideFirestore, getFirestore } from '@angular/fire/firestore'
 import { AngularFireModule } from '@angular/fire/compat'
-import { FetchCollectionByRouteDirective } from './shared/directives/fetch-collection-by-route/fetch-collection-by-route.directive';
+import { FetchCollectionByRouteDirective } from './shared/directives/fetch-collection-by-route/fetch-collection-by-route.directive'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { LayoutModule } from '@angular/cdk/layout'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
     declarations: [
@@ -48,6 +54,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
         provideAuth(() => getAuth()),
         provideFirestore(() => getFirestore()),
         BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
--- src/app/shared/components/page-frame/page-frame.component.ts
+++ src/app/shared/components/page-frame/page-frame.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
 import { User } from 'firebase/auth'
+import { Observable } from 'rxjs'
+import { map, shareReplay } from 'rxjs/operators'
 import { NavigationLink } from 'src/app/const'
 import { AuthService } from '../../services/auth/auth.service'
 
@@ -9,9 +12,18 @@ import { AuthService } from '../../services/auth/auth.service'
     styleUrls: ['./page-frame.component.scss'],
 })
 export class PageFrameComponent implements OnInit {
-    constructor(private _authService: AuthService) {}
+    constructor(
+        private _authService: AuthService,
+        private _breakpointObserver: BreakpointObserver
+    ) {}
     user: null | User = null
     navigationLinks = NavigationLink
+    isHandset$: Observable<boolean> = this._breakpointObserver
+        .observe(Breakpoints.Handset)
+        .pipe(
+            map((result) => result.matches),
+            shareReplay()
+        )
 
     logOut() {
         this._authService.logOut()
