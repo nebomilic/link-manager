cb1a502813be13f8b60f78428be7c04b1b0a7f56
Nebo Milic <nebo.milic@gmail.com>
2023-05-11 14:28:15 +0200

    Display different "add collection" buttons responsively

--- src/app/pages/my-collections/my-collections.component.ts
+++ src/app/pages/my-collections/my-collections.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core'
+import { Observable } from 'rxjs'
 import { NavigationLink } from 'src/app/const'
+import { BreakpointService } from 'src/app/shared/services/ui/breakpoint.service'
 
 @Component({
     selector: 'app-my-collections',
@@ -8,4 +10,10 @@ import { NavigationLink } from 'src/app/const'
 })
 export class MyCollectionsComponent {
     newCollectionLink = NavigationLink.NewCollection
+
+    constructor(private _breakpointService: BreakpointService) {}
+
+    get isHandset$(): Observable<boolean> {
+        return this._breakpointService.isHandset$
+    }
 }
