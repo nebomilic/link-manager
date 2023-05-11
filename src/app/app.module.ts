188a7ad932efdfd0d5e66054a02950105be2046b
Nebo Milic <nebo.milic@gmail.com>
2023-05-11 11:16:13 +0200

    Use material ui for collection list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
     declarations: [
@@ -60,6 +61,7 @@ import { MatListModule } from '@angular/material/list'
         MatSidenavModule,
         MatIconModule,
         MatListModule,
+        MatCardModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
