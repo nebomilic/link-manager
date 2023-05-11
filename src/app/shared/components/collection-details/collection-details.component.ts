dcda3b1a9bb4eb2ac8227025f5936b169f99fd46
Nebo Milic <nebo.milic@gmail.com>
2023-05-11 17:22:38 +0200

    Use material ui for collection details header

--- src/app/shared/components/collection-details/collection-details.component.ts
+++ src/app/shared/components/collection-details/collection-details.component.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Component } from '@angular/core'
+import { Observable } from 'rxjs'
 import { Collection } from 'src/app/types'
 import { AuthService } from '../../services/auth/auth.service'
+import { BreakpointService } from '../../services/ui/breakpoint.service'
 import { goToLink } from '../../utils'
 
 @Component({
@@ -13,7 +15,14 @@ export class CollectionDetailsComponent implements AfterViewInit {
     editable = false
     goToLink = goToLink
 
-    constructor(private _authService: AuthService) {}
+    constructor(
+        private _authService: AuthService,
+        private _breakpointService: BreakpointService
+    ) {}
+
+    get isHandset$(): Observable<boolean> {
+        return this._breakpointService.isHandset$
+    }
 
     ngAfterViewInit(): void {
         if (
