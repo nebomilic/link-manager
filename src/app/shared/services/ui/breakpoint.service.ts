64aca71d684287be9151f2562d7c664fcc20d64a
Nebo Milic <nebo.milic@gmail.com>
2023-05-11 13:42:47 +0200

    Add unsubscribe to BreakpointService

--- src/app/shared/services/ui/breakpoint.service.ts
+++ src/app/shared/services/ui/breakpoint.service.ts
@@ -1,18 +1,25 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
-import { Injectable } from '@angular/core'
-import { map, Observable, shareReplay } from 'rxjs'
+import { Injectable, OnDestroy } from '@angular/core'
+import { map, Observable, shareReplay, Subject, takeUntil } from 'rxjs'
 
 @Injectable({
     providedIn: 'root',
 })
-export class BreakpointService {
+export class BreakpointService implements OnDestroy {
+    private _destroy$ = new Subject<void>()
     public isHandset$: Observable<boolean> = new Observable()
     constructor(private _breakpointObserver: BreakpointObserver) {
         this.isHandset$ = this._breakpointObserver
             .observe(Breakpoints.Handset)
             .pipe(
+                takeUntil(this._destroy$),
                 map((result) => result.matches),
                 shareReplay()
             )
+    }
+
+    ngOnDestroy() {
+        this._destroy$.next()
+        this._destroy$.complete()
     }
 }
