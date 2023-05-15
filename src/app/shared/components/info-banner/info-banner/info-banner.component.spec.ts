43fe5452153b2693b155dfca60a798703d9e07fe
Nebo Milic <nebo.milic@gmail.com>
2023-05-15 08:49:50 +0200

    Create info banner component

---
+++ src/app/shared/components/info-banner/info-banner/info-banner.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InfoBannerComponent } from './info-banner.component';
+
+describe('InfoBannerComponent', () => {
+  let component: InfoBannerComponent;
+  let fixture: ComponentFixture<InfoBannerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InfoBannerComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InfoBannerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
