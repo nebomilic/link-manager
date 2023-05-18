048daa66a0fd0552feea471aa2646d26df91649b
Nebo Milic <nebo.milic@gmail.com>
2023-05-18 14:59:26 +0200

    Improve UI for mobile devices

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Observable } from 'rxjs'
+import { BreakpointService } from '../../services/ui/breakpoint/breakpoint.service'
 
 @Component({
-  selector: 'app-page-header',
-  templateUrl: './page-header.component.html',
-  styleUrls: ['./page-header.component.scss']
+    selector: 'app-page-header',
+    templateUrl: './page-header.component.html',
+    styleUrls: ['./page-header.component.scss'],
 })
 export class PageHeaderComponent {
+    constructor(private _breakpointService: BreakpointService) {}
 
+    get isHandset$(): Observable<boolean> {
+        return this._breakpointService.isHandset$
+    }
 }
