d7fa3c2534b741e6048fdcd8f48a63a2c1429fad
Nebo Milic <nebo.milic@gmail.com>
2023-05-19 08:16:09 +0200

    Remove unused dependency

--- src/app/shared/components/collection-list/collection-list.component.ts
+++ src/app/shared/components/collection-list/collection-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, ContentChild, Input, TemplateRef } from '@angular/core'
 import { Collection } from 'src/app/types'
-import { CollectionService } from '../../services/collection/collection.service'
 
 @Component({
     selector: 'app-collection-list[collections]', // makes "collections" mandatory
@@ -8,8 +7,6 @@ import { CollectionService } from '../../services/collection/collection.service'
     styleUrls: ['./collection-list.component.scss'],
 })
 export class CollectionListComponent {
-    constructor(private _collectionService: CollectionService) {}
-
     @Input() collections: Collection[] | null = []
     @Input() emptyDescription = ''
     @Input() emptyHint = ''
