74f1d518f73d4be7ca48be65f33d97cc3e49bb67
Nebo Milic <nebo.milic@gmail.com>
2024-09-12 17:41:57 +0200

    Remove secrets from config

--- environment.ts
+++ environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: 'AIzaSyAPO7DeO2zQypNRK5Pf_7WsmTXvoYbbm90',
-        authDomain: 'link-manager-e73c6.firebaseapp.com',
-        projectId: 'link-manager-e73c6',
-        storageBucket: 'link-manager-e73c6.appspot.com',
-        messagingSenderId: '979026681591',
-        appId: '1:979026681591:web:a66fbe0d0477a6371e3df2',
-        measurementId: 'G-R0CC65SGPS',
+        apiKey: 'xxx',
+        authDomain: 'xxx',
+        projectId: 'xxx',
+        storageBucket: 'xxxx',
+        messagingSenderId: 'xxx',
+        appId: 'xxx',
+        measurementId: 'xxx',
     },
 }
